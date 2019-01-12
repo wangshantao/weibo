1bbe4ddf9bfce490fc0bb5b167c33f0252552e64
tao <605682551@qq.com>
2019-01-12 16:28:55 +0800

    4.2 美化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>hello laravel</h1>
+        <p class="lead">
+            you can see
+        </p>
+        <p>
+            now it's begun
+        </p>
+        <p>
+            <a href="#" role="button" class="btn btn-lg btn-success">register now</a>
+        </p>
+    </div>
 @stop
