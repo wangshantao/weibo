0e239895111f45a0eefd934053d97cc1c6ac39cb
tao <605682551@qq.com>
2019-01-26 20:54:45 +0800

    头部界面和用户界面

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+
+Route::get('signup', 'UsersController@create');
+Route::resource('users', 'UsersController');
