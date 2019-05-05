d0c62751fc2caf6591485a6cdd63818faba1c217
tao <605682551@qq.com>
2019-05-05 18:40:12 +0800

    用户激活

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
